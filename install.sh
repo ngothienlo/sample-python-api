9a36d163bd1cc23e6ea7bbbde9938864443dc8fa
Ngoc Luc <lucpn@trobz.com>
2019-06-13 13:27:26 +0700

    [REF] - UPDATE Install file

--- install.sh
+++ install.sh
@@ -8,10 +8,14 @@ sudo apt-get install build-essential python-pip libffi-dev python-dev python3-de
 
 # install postgres
 apt-get install -y postgresql
+
+sudo cp ./db_conf/* /etc/postgresql/11/main
+
 # echo "CREATE ROLE deploy LOGIN ENCRYPTED PASSWORD '$APP_DB_PASS';" | sudo -u postgres psql
 sudo -u postgres bash -c "psql -c \"CREATE USER assign_user WITH PASSWORD 'assign_123';\""
-su postgres -c "createdb assignment_api --owner assign_user"
+sudo su postgres -c "createdb assignment_api --owner assign_user"
 service postgresql reload
+
 
 
 # check venv
@@ -22,3 +26,9 @@ rm -rf .demo_api
 virtualenv -p python3 .demo_api
 source .demo_api/bin/activate
 pip install -r requirements.txt
+
+source .demo_api/bin/activate
+python3 ./app/models.py
+
+
+echo "Install required packages finished"
